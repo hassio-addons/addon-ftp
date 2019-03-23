10af1020cd2727b70d17bc0d840af5a6ea6ccc80
Franck Nijhof <frenck@addons.community>
2019-03-23 21:18:55 +0100

    :hammer: Rewrite add-on onto Bashio

--- ftp/rootfs/etc/cont-init.d/50-vsftpd.sh
+++ ftp/rootfs/etc/cont-init.d/50-vsftpd.sh
@@ -1,47 +1,44 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: FTP
 # Configures vsftpd
 # ==============================================================================
-# shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
-
 readonly CONFIG=/etc/vsftpd/vsftpd.conf
 
 # Configure ports
 {
-    echo "listen_port=$(hass.config.get 'port')"
-    echo "ftp_data_port=$(hass.config.get 'data_port')"
+    echo "listen_port=$(bashio::config 'port')"
+    echo "ftp_data_port=$(bashio::config 'data_port')"
 } >> "${CONFIG}"
 
 # Passive mode
-if hass.config.true 'pasv'; then
+if bashio::config.true 'pasv'; then
     {
         echo 'pasv_enable=YES'
-        echo "pasv_min_port=$(hass.config.get 'pasv_min_port')"
-        echo "pasv_max_port=$(hass.config.get 'pasv_max_port')"
+        echo "pasv_min_port=$(bashio::config 'pasv_min_port')"
+        echo "pasv_max_port=$(bashio::config 'pasv_max_port')"
     } >> "${CONFIG}"
-    
+
     # Has the user configured a passive address?
-    if hass.config.has_value 'pasv_address'; then
-        echo "pasv_address=$(hass.config.get 'pasv_address')" >> "${CONFIG}"
+    if bashio::config.has_value 'pasv_address'; then
+        echo "pasv_address=$(bashio::config 'pasv_address')" >> "${CONFIG}"
     fi
 else
     echo 'pasv_enable=NO' >> "${CONFIG}"
 fi
 
 # SSL
-if hass.config.true 'ssl'; then
+if bashio::config.true 'ssl'; then
     {
         echo 'ssl_enable=YES'
         echo 'ssl_sslv2=NO'
         echo 'ssl_sslv3=NO'
         echo 'ssl_tlsv1=YES'
-        echo "rsa_cert_file=/ssl/$(hass.config.get 'certfile')"
-        echo "rsa_private_key_file=/ssl/$(hass.config.get 'keyfile')"
+        echo "rsa_cert_file=/ssl/$(bashio::config 'certfile')"
+        echo "rsa_private_key_file=/ssl/$(bashio::config 'keyfile')"
     } >> "${CONFIG}"
 
-    if hass.config.true 'implicit_ssl'; then
+    if bashio::config.true 'implicit_ssl'; then
         echo 'implicit_ssl=YES' >> "${CONFIG}"
     else
         echo 'implicit_ssl=NO' >> "${CONFIG}"
@@ -51,15 +48,15 @@ else
 fi
 
 # Debug mode
-if hass.debug; then
+if bashio::debug; then
     echo 'log_ftp_protocol=YES' >> "${CONFIG}"
-    if hass.config.true 'ssl'; then
+    if bashio::config.true 'ssl'; then
         echo 'debug_ssl=YES' >> "${CONFIG}"
     fi
 fi
 
 # Other settings
 {
-    echo "ftpd_banner=$(hass.config.get 'banner')"
-    echo "max_clients=$(hass.config.get 'max_clients')"
+    echo "ftpd_banner=$(bashio::config 'banner')"
+    echo "max_clients=$(bashio::config 'max_clients')"
 } >> "${CONFIG}"
--- ftp/rootfs/etc/cont-init.d/51-users.sh
+++ ftp/rootfs/etc/cont-init.d/51-users.sh
@@ -1,50 +1,46 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: FTP
 # Configures vsftpd
 # ==============================================================================
-# shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
-
 declare username
 declare password
 
-for user in $(hass.config.get 'users|keys[]'); do
-    username=$(hass.config.get "users[${user}].username")
+for user in $(bashio::config 'users|keys'); do
+    username=$(bashio::config "users[${user}].username")
 
     mkdir -p "/ftproot/users/${username}"
     touch "/etc/vsftpd/users/${username}"
 
     for dir in "addons" "backup" "config" "share" "ssl"; do
-        if hass.config.true "users[${user}].${dir}"; then
+        if bashio::config.true "users[${user}].${dir}"; then
             mkdir "/ftproot/users/${username}/${dir}"
             mount --bind "/${dir}" "/ftproot/users/${username}/${dir}"
         fi
     done
 
-    if hass.config.true "users[${user}].allow_chmod"; then
+    if bashio::config.true "users[${user}].allow_chmod"; then
         echo 'chmod_enable=YES' >> "/etc/vsftpd/users/${username}"
     fi
 
-    if hass.config.true "users[${user}].allow_download"; then
+    if bashio::config.true "users[${user}].allow_download"; then
         echo 'download_enable=YES' >> "/etc/vsftpd/users/${username}"
     fi
 
-    if hass.config.true "users[${user}].allow_upload"; then
+    if bashio::config.true "users[${user}].allow_upload"; then
         echo 'write_enable=YES' >> "/etc/vsftpd/users/${username}"
     fi
 
-    if hass.config.true "users[${user}].allow_dirlist"; then
+    if bashio::config.true "users[${user}].allow_dirlist"; then
         echo 'dirlist_enable=YES' >> "/etc/vsftpd/users/${username}"
     fi
 
     # Require a secure password
-    if hass.config.has_value "users[${user}].password" \
-        && ! hass.config.is_safe_password "users[${user}].password"; then
-        hass.die \
-          "Please choose a different pass for ${username}, this one is unsafe!"
+    if bashio::config.has_value "users[${user}].password" \
+        && ! bashio::config.true 'i_like_to_be_pwned'; then
+        bashio::config.require.safe_password "users[${user}].password"
     fi
 
-    password=$(hass.config.get "users[${user}].password" | openssl passwd -1 -stdin)
+    password=$(bashio::config "users[${user}].password" | openssl passwd -1 -stdin)
     echo "${username}:${password}" >> /etc/vsftpd/passwd
 done
